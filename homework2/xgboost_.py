536f08d14ba9860f7373063a805597b8ec227047
Andie <zchangan@163.com>
2018-07-08 08:49:09 +0800

    xgboost

--- homework2/xgboost_.py
+++ homework2/xgboost_.py
@@ -3,22 +3,24 @@ import pandas as pd
 import xgboost as xgb
 
 dtest = xgb.DMatrix('./dataset/test.txt')
-dtrain = xgb.DMatrix('./dataset/train.txt')
+dtrain_ = xgb.DMatrix('./dataset/train.txt')
 
-rows = dtrain.num_row()
+rows = dtrain_.num_row()
 validation_rate = 0.08
 indices = list(range(0, rows))
 random.shuffle(indices)
 bound_index = int(rows * validation_rate)
 validation_index = indices[:bound_index]
 train_index = indices[bound_index:]
-dvalidate = dtrain.slice(validation_index)
-dtrain = dtrain.slice(train_index)
+dvalidate = dtrain_.slice(validation_index)
+dtrain = dtrain_.slice(train_index)
 
 param = {'objective': 'binary:logistic'}
 
 bst = xgb.train(param, dtrain, num_boost_round=10000, evals=[
                 (dvalidate, 'eval')], early_stopping_rounds=100)
+
+bst = xgb.train(param, dtrain_, num_boost_round=bst.best_iteration)
 
 result = bst.predict(dtest)
 pd.Series(result).to_csv(
